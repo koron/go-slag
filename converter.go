9cd785ee85377ea341a1885550ac8b8277379129
MURAOKA Taro <koron.kaoriya@gmail.com>
2015-02-02 02:26:29 +0900

    implement bool converter

--- converter.go
+++ converter.go
@@ -2,6 +2,21 @@ package slag
 
 import "reflect"
 
-type converter interface {
-	convert(args []string, dest *reflect.Value) (used int, err error)
+type converter func(args []string, dest *reflect.Value) (used int, err error)
+
+func findConverter(t reflect.Type) (c converter, err error) {
+	k := t.Kind()
+	// TODO: support other types.
+	switch k {
+	case reflect.Bool:
+		return boolConverter, nil
+	default:
+		return nil, ErrorSlag{message: "not supported kind: " + k.String()}
+	}
+	return
+}
+
+func boolConverter(args []string, dest *reflect.Value) (used int, err error) {
+	dest.SetBool(true)
+	return 0, nil
 }
